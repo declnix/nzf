6fa2fc7c6ad7c5b0f3b0a884a9e5fa77fa209652
declnix <20567435+declnix@users.noreply.github.com>
2026-03-11 07:36:46 +0100

    feat(zsh-autosuggestions): add home-manager compatible alias

--- homeModule/plugins/zsh-autosuggestions.nix
+++ homeModule/plugins/zsh-autosuggestions.nix
@@ -6,16 +6,18 @@
 }:
 let
   inherit (lib) mkEnableOption mkIf mkDefault;
-  nzf = import ../../lib {inherit lib;};
+  nzf = import ../../lib { inherit lib; };
   inherit (nzf) entryAfter defer plugin;
   cfg = config.programs.nzf.zsh-autosuggestions;
 in
 {
   options.programs.nzf.zsh-autosuggestions.enable = mkEnableOption "zsh-autosuggestions";
+  options.programs.nzf.autosuggestion.enable = mkEnableOption "zsh-autosuggestions";
 
-  config = mkIf cfg.enable {
+  config = mkIf (cfg.enable || config.programs.nzf.autosuggestion.enable) {
     programs.nzf.zsh-defer.enable = mkDefault true;
-    programs.nzf.plugins.zsh-autosuggestions =
-      entryAfter ["zsh-defer"] (defer (plugin pkgs.zsh-autosuggestions));
+    programs.nzf.plugins.zsh-autosuggestions = entryAfter [ "zsh-defer" ] (
+      defer (plugin pkgs.zsh-autosuggestions)
+    );
   };
 }
