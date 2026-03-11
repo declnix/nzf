112b7c412716f282dbdd329c21b34c0f748ae066
declnix <20567435+declnix@users.noreply.github.com>
2026-03-11 07:36:42 +0100

    feat(zsh-fzf-history-search): add plugin

---
+++ homeModule/plugins/zsh-fzf-history-search.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  inherit (lib) mkEnableOption mkIf mkDefault;
+  nzf = import ../../lib { inherit lib; };
+  inherit (nzf) entryAfter defer plugin;
+  cfg = config.programs.nzf.zsh-fzf-history-search;
+in
+{
+  options.programs.nzf.zsh-fzf-history-search.enable = mkEnableOption "zsh-fzf-history-search";
+
+  config = mkIf cfg.enable {
+    programs.nzf.zsh-defer.enable = mkDefault true;
+    home.packages = [ pkgs.fzf ];
+    programs.nzf.plugins.zsh-fzf-history-search = entryAfter [ "zsh-defer" ] (
+      defer (plugin pkgs.zsh-fzf-history-search)
+    );
+  };
+}
