ce23181618e43bc896f70dba2515e764e3344419
declnix <20567435+declnix@users.noreply.github.com>
2026-03-11 11:24:19 +0100

    style(omz): remove imports
    we use import-tree that imports all of files from home module directly

--- homeModule/plugins/oh-my-zsh/default.nix
+++ homeModule/plugins/oh-my-zsh/default.nix
@@ -9,18 +9,8 @@ let
     filterAttrs
     hasSuffix
     ;
-
-  importDir =
-    dir:
-    let
-      files = builtins.readDir dir;
-      nixFiles = filterAttrs (name: type: type == "regular" && hasSuffix ".nix" name) files;
-    in
-    map (name: dir + "/${name}") (builtins.attrNames nixFiles);
 in
 {
-  imports = importDir ./lib ++ importDir ./plugins;
-
   options.programs.nzf.oh-my-zsh.plugins = mkOption {
     type = types.listOf types.str;
     default = [ ];
